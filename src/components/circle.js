7b8589722cb1a0ab09892c0b5aec6eeaa20a4cab
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2023-01-07 17:52:20 +0530

    commit

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,17 @@
 // import using require
+let Shape = require('./shape');
 
 // declare class
+class Circle extends Shape{
+    constructor(name){
+        super(name);
+    }
+
+    calculateArea(radius){
+        return Math.PI * Math.pow(radius, 2)
+    }
+}
 
 
-// export class using module.exports
+// export class using module.exports 
+module.exports=Circle; 
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,19 @@
 // declare class
+class Shape{
+    constructor(name){
+        this.name = name;
+    }
+
+    drawShape(){
+        return `${this.name} is draw`;
+    }
+    calculateArea(length, width){
+        return 2*(length * width);
+    }
+    color(color){
+        return `${color} is Applied on ${this.name}`
+    }
+}
 
 // export class using module.exports
+module.exports = Shape;
